3945e28b634aa80a04c06e4f07c5d071e0964039
Natthapong Sukkleang <Toon.automation@gmail.com>
2025-08-10 19:07:28 +0700

    Update All and Test

--- src/TagStatic.h
+++ src/TagStatic.h
@@ -48,6 +48,30 @@ enum isMemory : uint8_t
 };
 #pragma pack(pop)
 
+static void * Allocator(isMemory memType, size_t elements, size_t elementSize)
+{
+    switch (memType)
+    {
+    case isMemory::RAM:
+        return _SECTOR_RAM_(elements, elementSize);
+        break;
+    case isMemory::PSRAM:
+        return _SECTOR_PSRAM_(elements, elementSize);
+        break;
+    case isMemory::SPARE_1:
+        return _SECTOR_SPARE1_(elements, elementSize);
+        break;
+    case isMemory::SPARE_2:
+        return _SECTOR_SPARE2_(elements, elementSize);
+        break;
+    default:
+        return _SECTOR_RAM_(elements, elementSize);
+        break;
+    }
+    return nullptr;
+}
+
+
 #pragma pack(push,1)
 enum isType : uint8_t
 {
@@ -87,11 +111,11 @@ static char PrintOut[MAX_BUFFER_PRINT];
 typedef size_t (*PrintPointData)(size_t ,void *,size_t);
 size_t static DISP_CHAR(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%c",((char*)data)[index]);}
 size_t static DISP_INT8(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%d ",((int8_t*)data)[index]);}
-size_t static DISP_UINT8(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%d ",((uint8_t*)data)[index]);}
+size_t static DISP_UINT8(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%u ",((uint8_t*)data)[index]);}
 size_t static DISP_INT16(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%d ",((int16_t*)data)[index]);}
-size_t static DISP_UINT16(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%d ",((uint16_t*)data)[index]);}
+size_t static DISP_UINT16(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%u ",((uint16_t*)data)[index]);}
 size_t static DISP_INT32(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%d ",((int32_t*)data)[index]);}
-size_t static DISP_UINT32(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%d ",((uint32_t*)data)[index]);}
+size_t static DISP_UINT32(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%u ",((uint32_t*)data)[index]);}
 size_t static DISP_FLOAT(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%.3f ",((float*)data)[index]);}
 size_t static DISP_DOUBLE(size_t offset,void *data,size_t index){ return  snprintf(PrintOut + offset,MAX_BUFFER_PRINT - offset,"%.3f ",((double*)data)[index]);}
 
@@ -113,12 +137,14 @@ struct Tag
     public:
         RawMemory _name;       /**< Name of the component */
         RawMemory _data;       /**< Data buffer for the component */
+
+        Tag *_next = nullptr;
+        Tag *_first = nullptr;
+
         isMemory _memType:8;  /**< Memory type for the component */
         uint8_t _external_alloc:8;
         isType _type:8;        /**< Data type of the component */
 
-        Tag *_next = nullptr;
-        Tag *_first = nullptr;
 
         template<typename T>
         bool Set(T value,size_t index = 0)
@@ -132,12 +158,13 @@ struct Tag
             /* Check Elements Per Size is equal */
             monitor = (a == b);
             if(!monitor)
-                return !monitor;
+                return monitor;
 
             /* Check is not over index */
-            monitor = ((_data.size / SchematicPoint[_type].element_size) < index);
-            if(monitor)
-                return monitor;
+            b = GetArraySize();
+            monitor = ((b-1) >= index);
+            if(!monitor)
+                return false;
             
             T *tmp = (T*)_data.value; 
 
@@ -155,8 +182,8 @@ struct Tag
             if(!monitor)
                 return T{};
 
-            monitor = (GetArraySize() < index);
-            if(monitor)
+            monitor = (GetArraySize()-1 >= index);
+            if(!monitor)
                 return T{};
             
             T *tmp = (T*)_data.value; 
@@ -189,7 +216,7 @@ struct Tag
         {
             bool monitor;
 
-            monitor = (_name.value != nullptr);
+            monitor = ((_name.size > 0) && (_name.value != nullptr));
             if(monitor)
             {
                 free(_name.value);
@@ -199,23 +226,40 @@ struct Tag
             _name.size = strlen(name) + 1;
             
             _name.value = (uint8_t*)Allocator(_memType,_name.size,sizeof(uint8_t));
-            monitor = (_name.value != nullptr);
+            monitor = (_name.value == nullptr);
             if(monitor)
                 return !monitor;
 
             memcpy(_name.value,name,_name.size-1);
             _name.value[_name.size-1] = MARK_CHAR_NULL;
 
-            return monitor;
+            return true;
+        }
+      
+        const char * GetName()
+        {
+            return (char*)_name.value;
         }
 
         void Free()
         {
             if(_name.value != nullptr)
+            {
+                //memset(_name.value,MARK_FREE_MEMORY,_name.size);
+                //memset(&_name.size,MARK_FREE_MEMORY,sizeof(_name.size));
                 free(_name.value);
+            }
 
             if((_data.value != nullptr) && (_external_alloc == 0))
-                free(_name.value);
+            {
+                //memset(_data.value,MARK_FREE_MEMORY,_data.size);
+                //memset((uint8_t*)&_data.size,MARK_FREE_MEMORY,sizeof(_data.size));
+                free(_data.value);
+            }
+            
+           // memset((uint8_t*)_memType,MARK_FREE_MEMORY,sizeof(isMemory));
+           // memset((uint8_t*)_external_alloc,MARK_FREE_MEMORY,sizeof(isMemory));
+           // memset((uint8_t*)_type,MARK_FREE_MEMORY,sizeof(isMemory));
             
             _first = nullptr;
             _next = nullptr;
@@ -223,7 +267,7 @@ struct Tag
 };
 #pragma pack(pop)
 
-struct TagGroup : Tag
+struct TagGroup
 {
     private:
         Tag *_Item = nullptr ; 
@@ -231,6 +275,10 @@ struct TagGroup : Tag
     public:
         Tag * Add(isType type,const char * name,size_t array_size=1,isMemory memtype = isMemory::RAM)
         {
+
+            if(array_size == 0) 
+                return nullptr;
+
             bool monitor;
             Tag **cur = &_Item;
             Tag **cur_first = &_Item;
@@ -246,6 +294,9 @@ struct TagGroup : Tag
             monitor = (newItem != nullptr);
             if(!monitor)
             return nullptr;
+
+            newItem->_name.size = 0;
+            newItem->_data.size = 0;
 
             newItem->_data.size = SchematicPoint[type].element_size * array_size;
             newItem->_memType = memtype;
@@ -267,11 +318,11 @@ struct TagGroup : Tag
             return *cur;
         }
 
-
-
-
         Tag * Add(isType type,const char * name,void *addr,size_t array_size=1,isMemory memtype = isMemory::RAM)
         {
+            if(array_size == 0)
+                return nullptr;
+
             bool monitor;
             Tag **cur = &_Item;
             Tag **cur_first = &_Item;
@@ -287,6 +338,9 @@ struct TagGroup : Tag
             monitor = (newItem != nullptr);
             if(!monitor)
             return nullptr;
+
+            newItem->_name.size = 0;
+            newItem->_data.size = 0;
 
             newItem->_data.size = SchematicPoint[type].element_size * array_size;
             newItem->_memType = memtype;
@@ -308,55 +362,379 @@ struct TagGroup : Tag
             return *cur;
         }
 
-        bool Remove(size_t index);
-        void Remove(const char * name);
+        Tag * Find(const char *name)
+        {
+            bool monitor;
+            Tag *cur = _Item;
+
+            while(cur != nullptr)
+            {
+                monitor = (strcmp(name,(char*)cur->_name.value) == 0);
+                if(monitor)
+                    return cur;
+
+                cur = cur->_next;
+            }
+
+            return nullptr;
+        }
+
+        Tag * Find(size_t index)
+        {
+            bool monitor;
+            size_t count = 0;
+            Tag **cur = &_Item;
+
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {
+                monitor = (index == count);
+                if(monitor)
+                    return *cur;
+                    
+                cur = &(*cur)->_next;
+                count+=1;
+            }
+
+            return nullptr;
+        }
+
+        int FindIndex(const char *name)
+        {
+            int count = 0;
+            bool monitor;
+            Tag **cur = &_Item;
+
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {
+                monitor = (strcmp(name,(char*)(*cur)->_name.value) == 0);
+                if(monitor)
+                    return count;
+
+                cur = &(*cur)->_next;
+                count+=1;
+            }
+
+            return -1;
+        }
+
+        void Remove(size_t index)
+        {
+            bool monitor;
+            int count = 0;
+            Tag **cur_prev = nullptr;
+            Tag **cur = &_Item;
+
+
+            while((cur != nullptr)&&((*cur) != nullptr))
+            {
+                Tag *tmp = nullptr;
+
+                monitor = (count == index);
+                if(monitor)
+                {
+                    tmp = (*cur)->_next;
+                    (*cur)->Free();
+                    free(*cur);
+                    *cur = tmp;
+
+                    if((cur_prev != nullptr) && ((*cur_prev) != nullptr))
+                        (*cur_prev)->_next = *cur;
+
+                    return ;
+                }
+
+                cur_prev = cur;
+                cur = &(*cur)->_next; 
+                count++;
+            }
+        }
+
+        void Remove(const char * name)
+        {
+           int index = FindIndex(name);
+
+           if(index == -1) return;
+
+           Remove(index);
+        }
+
+        void RemoveAll()
+        {
+            Tag **cur = &_Item;
+
+            
+            while((cur != nullptr) && ((*cur) != nullptr ))
+            {
+                Tag **tmp = nullptr;
+                tmp = cur;
+                *cur = (*cur)->_next;
+                (*tmp)->Free();
+                free(*tmp);
+                *tmp = nullptr;
+            }
+        }
+
         size_t Count()
         {
+            Tag **cur = &_Item;
+            size_t count = 0;
             
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {   
+                cur = &(*cur)->_next;
+                count+=1;
+            }
+
+            return count;
         }
-};
+
+    };
 
 #pragma pack(push,1)
 struct Topic
 {
-    RawMemory name;       /**< Name of the group */
-    isMemory memType;
-    TagGroup * Group = nullptr;
-    Topic *Home = nullptr ;      /**< Pointer to the first component in the list */
-    Topic *Next = nullptr ;       /**< Pointer to the home component in the list */
+    RawMemory _name;       /**< Name of the group */
+    isMemory _memType;
+    TagGroup * _group = nullptr;
+    Topic *_first = nullptr;
+    Topic *_next = nullptr;
 
     const char * MonitorInfo()
     {
         size_t out = 0;
-        out = sprintf(&PrintOut[0],"TOPIC %s\n",name.value);
+        out = sprintf(&PrintOut[0],"TOPIC %s\n",_name.value);
         return &PrintOut[0];
+    }
+  
+    const char * Getname()
+    {
+        return (char*)_name.value;
+    }
+    
+    bool SetName(const char *name)
+    {
+        bool monitor;
+
+        monitor = ((_name.value != nullptr) && (_name.size > 0));
+        if(monitor)
+        {
+            free(_name.value);
+            _name.value = nullptr;
+        }
+
+        _name.size = strlen(name) + 1;
+        
+        _name.value = (uint8_t*)Allocator(_memType,_name.size,sizeof(uint8_t));
+        monitor = (_name.value != nullptr);
+        if(!monitor)
+            return false;
+
+
+
+        memcpy(_name.value,name,_name.size-1);
+        _name.value[_name.size-1] = MARK_CHAR_NULL;
+
+        return true;
+    }
+
+    void Free()
+    {        
+        if((_name.value != nullptr) && (_name.size > 0))
+        {
+            //memset(_name.value,MARK_FREE_MEMORY,_name.size);
+            //memset(&_name.size,MARK_FREE_MEMORY,sizeof(_name.size));
+            free(_name.value);
+            _name.value = nullptr;
+        }
+
+        if(_group != nullptr )
+        {
+            _group->RemoveAll();
+            free(_group);
+            _group = nullptr;
+        }
+
+        _first = nullptr;
+        _next = nullptr;
+    }
+    
+    TagGroup * Items()
+    {
+        return _group;
     }
 
 };
 #pragma pack(pop)
 
-
-static void * Allocator(isMemory memType, size_t elements, size_t elementSize)
+#pragma pack(push,1)
+struct TopicGroup
 {
-    switch (memType)
-    {
-    case isMemory::RAM:
-        return _SECTOR_RAM_(elements, elementSize);
-        break;
-    case isMemory::PSRAM:
-        return _SECTOR_PSRAM_(elements, elementSize);
-        break;
-    case isMemory::SPARE_1:
-        return _SECTOR_SPARE1_(elements, elementSize);
-        break;
-    case isMemory::SPARE_2:
-        return _SECTOR_SPARE2_(elements, elementSize);
-        break;
-    default:
-        return _SECTOR_RAM_(elements, elementSize);
-        break;
-    }
-    return nullptr;
-}
+    Topic * _Topics = nullptr ;
+    
+        Topic * Add(const char * name,isMemory memtype = isMemory::RAM)
+        {
+        
+
+            bool monitor;
+            Topic **cur = &_Topics;
+            Topic **cur_first = &_Topics;
+
+            /* 1. Find last elements for connect */
+            while ((cur != nullptr) && (*cur != nullptr))
+            {
+                cur = &(*cur)->_next;
+            }
+
+            Topic *newTopic = (Topic*)Allocator(memtype,1,sizeof(Topic));
+
+            monitor = (newTopic != nullptr);
+            if(!monitor)
+            return nullptr;
+
+            newTopic->_memType = memtype;
+            newTopic->_group = nullptr;
+        
+            monitor = newTopic->SetName(name);
+            if(!monitor)
+            return nullptr;
+
+            *cur = newTopic;
+            (*cur)->_first = (*cur_first);
+
+            return *cur;
+        }
+
+        Topic * Find(const char *name)
+        {
+            bool monitor;
+            Topic **cur = &_Topics;
+
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {
+                monitor = (strcmp(name,(char*)(*cur)->_name.value) == 0);
+                if(monitor)
+                    return *cur;
+
+                cur = &(*cur)->_next;
+            }
+
+            return nullptr;
+        }
+
+        Topic * Find(size_t index)
+        {
+            bool monitor;
+            size_t count = 0;
+            Topic **cur = &_Topics;
+
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {
+                monitor = (index == count);
+                if(monitor)
+                    return *cur;
+                    
+                cur = &(*cur)->_next;
+                count+=1;
+            }
+
+            return nullptr;
+        }
+
+        int FindIndex(const char *name)
+        {
+            int count = 0;
+            bool monitor;
+            Topic **cur = &_Topics;
+
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {
+                monitor = (strcmp(name,(char*)(*cur)->_name.value) == 0);
+                if(monitor)
+                    return count;
+
+                cur = &(*cur)->_next;
+                count+=1;
+            }
+
+            return -1;
+        }
+
+        void Remove(size_t index)
+        {
+            bool monitor;
+            int count = 0;
+            Topic **cur_prev = nullptr;
+            Topic **cur = &_Topics;
+
+
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {
+                Topic *tmp = nullptr;
+                monitor = (count == index);
+                if(monitor)
+                {
+                     tmp = (*cur)->_next;
+                    (*cur)->Free();
+                    free(*cur);
+                    *cur = tmp;
+
+                    if((cur_prev != nullptr) && ((*cur_prev) != nullptr))
+                        (*cur_prev)->_next = *cur;
+
+                    return ;
+                }
+
+                cur_prev = cur;
+                cur = &(*cur)->_next; 
+                count++;
+            }
+        }
+
+        void Remove(const char * name)
+        {
+           int index = FindIndex(name);
+
+           if(index == -1) return;
+
+           Remove(index);
+        }
+
+        void RemoveAll()
+        {
+            Topic **cur = &_Topics;
+            
+            while((*cur != nullptr) && ((*cur) != nullptr))
+            {
+                Topic **tmp = nullptr;
+                tmp = cur;
+                *cur = (*cur)->_next;
+                if( (*tmp) != nullptr)
+                {
+                    (*tmp)->Free();
+                    free(*tmp);
+                    *tmp = nullptr; 
+                }
+            }
+        }
+
+        size_t Count()
+        {
+            Topic **cur = &_Topics;
+            size_t count = 0;
+            
+            while((cur != nullptr) && ((*cur) != nullptr))
+            {   
+                cur = &(*cur)->_next;
+                count+=1;
+            }
+
+            return count;
+        }
+    
+};
+#pragma pack(pop)
+
+
+
 
 #endif // TAGSTATIC_H
